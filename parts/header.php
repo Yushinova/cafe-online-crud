53e2d39908d2b8373eaa356b58fe502c21c50004
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2025-06-13 15:00:44 +0300

    add href to home

--- parts/header.php
+++ parts/header.php
@@ -1,32 +1,33 @@
 
 <header>
+    <a href="/">
         <div class="logo">
             <img src="./../resource/images/лого.jpg" alt="Логотип">
         </div>
-        <div class="deliver-div">
-             <div class="deliver">
-                 <img class="deliver" src="./../resource/images/deliver.png" alt="Логотип">
-             </div>
-             <div class="phone">
-                  +7 (123) 456-78-90
-              </div>
+    </a> 
+    <div class="deliver-div">
+        <div class="deliver">
+             <img class="deliver" src="./../resource/images/deliver.png" alt="Логотип">
         </div>
-       <a href="/pages/cart.php">
-            <div class="cart">
-                 <img src="./../resource/images/cart.png" alt="Корзина">
-                 <span>3</span>
-             </div>
-        </a>
-        <nav>
-            <ul>
-                <li><a href="./../pages/cafe.php">Меню</a></li>
-                <li><a href="#">Пицца</a></li>
-                <li><a href="#">Бургер</a></li>
-                <li><a href="#">Шаурма</a></li>
-                <li><a href="#">Салаты</a></li>
-                <li><a href="#">Напитки</a></li>
-            </ul>
-        </nav>
-        
-    </header>
+        <div class="phone">
+             +7 (123) 456-78-90
+        </div>
+    </div>
+    <a href="/pages/cart.php">
+        <div class="cart">
+            <img src="./../resource/images/cart.png" alt="Корзина">
+            <span>3</span>
+        </div>
+    </a>
+    <nav>
+         <ul>
+            <li><a href="./../pages/cafe.php">Меню</a></li>
+            <li><a href="#">Пицца</a></li>
+            <li><a href="#">Бургер</a></li>
+            <li><a href="#">Шаурма</a></li>
+            <li><a href="#">Салаты</a></li>
+            <li><a href="#">Напитки</a></li>
+        </ul>
+    </nav>       
+</header>
     
